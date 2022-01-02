97d0a3379203be6ada975e560ae78e9f9cf674f3
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-01-02 10:43:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.button')
+
+btn.addEventListener('dblclick', function(e){
+    btn.classList.toggle('aaa')
+})
+
+// let userClickCounter = 0
+// function watchUserClicks(){
+//     userClickCounter++;
+//     console.log(`User Clicked${userClickCounter}`)
+// }
+
+// function watchUserClicks(){
+   
+//     console.count(`User Clicked`)
+// }
+
+
+
+
+
